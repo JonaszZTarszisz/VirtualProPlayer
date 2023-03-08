3331b31decec5057fa295d2ee7e55a16941f2f44
JonaszZTarszisz <108954281+JonaszZTarszisz@users.noreply.github.com>
2023-03-09 00:01:47 +0100

    next panel

--- public/app.js
+++ public/app.js
@@ -8,27 +8,32 @@ class ControlPanel {
     this.previousStep = document.querySelectorAll(".stepPrevious");
     this.nextStep = document.querySelectorAll(".stepNext");
     this.buffSkill = document.querySelectorAll(".buffSkill");
-    this.skillAreola = document.querySelectorAll(".skillAreola");
-
+    this.physcialPanel = document.getElementById("physcialPanel");
+    this.mainPageAttributes = document.getElementById("attributes")
+    this.mainPage = document.getElementById("mainPage");
+    
     this.positions = ["GK", "CB", "LB", "LWB", "RB", "RWB", "CDM", "CAM",
                          "CM", "LM", "LW", "RM", "RW", "ST", "CF", "LF", "RF"];
 
     this.frontPanel();
+    this.updatePanel();
+    this.previousMenu();
+    this.nextPanel();
     }
 
     frontPanel() {
         this.previousStep[0].addEventListener("click", () => {
-            const value = position.innerHTML;
-            let index = positions.indexOf(value);
-            if(index <= 0) index = positions.length;
-            position.innerHTML = positions[index-1];
+            const value = this.position.innerHTML;
+            let index = this.positions.indexOf(value);
+            if(index <= 0) index = this.positions.length;
+            this.position.innerHTML = this.positions[index-1];
         });
 
         this.nextStep[0].addEventListener("click", () => {
-            const value = position.innerHTML;
-            let index = positions.indexOf(value);
-            if(index >= positions.length - 1) index = -1;
-            position.innerHTML = positions[index+1];
+            const value = this.position.innerHTML;
+            let index = this.positions.indexOf(value);
+            if(index >= this.positions.length - 1) index = -1;
+            this.position.innerHTML = this.positions[index+1];
         });
 
         this.previousStep[1].addEventListener("click", () => {
@@ -54,7 +59,57 @@ class ControlPanel {
             if(value >= 115) value = 44;
             weight.innerHTML = +value + 1;
         });
-}
+    }
+
+    /*updatePanel() {
+        const physcialID = "#" + this.physcialPanel.getAttributeNode("id").value;
+        let checkboxes = Array.from(document.querySelectorAll(physcialID+ "> .buffPart > .storey > .buffSkill > .checkbox"));
+        console.log(checkboxes);
+        checkboxes.forEach(e => e.addEventListener("click", () => {
+            if(e.checked) {
+                let label = document.querySelector("#" + e.id + "~ label p");
+                console.log(label, e)
+            }
+        }))
+    }*/
+
+    updatePanel() {
+        this.updatePanelPage = document.getElementById("updatePanel");
+        this.mainPageAttributes.addEventListener('click', () => {
+            this.mainPage.classList.add("d-none");
+            this.updatePanelPage.classList.remove("d-none");
+        })
+    }
+
+    nextPanel() {
+        let navUpdatePanel = document.querySelectorAll("#navbarUpdatePanel > .nav-item > .nav-link");
+        let skillPanels = Array.from(document.querySelectorAll(".skillPanel"));
+        navUpdatePanel.forEach(e => e.addEventListener("click", () => {
+            console.log(skillPanels);
+            let activePanel = document.querySelector(".active-panel");
+            let namePanel = e.innerHTML.toLowerCase();
+            let focusPanel = skillPanels.filter( e => {
+                let idOfFocusOfPanel = e.getAttributeNode("id").value;
+                return idOfFocusOfPanel.slice(0, namePanel.length) == namePanel;
+            })
+        activePanel.classList.remove("active-panel");
+        activePanel.classList.add("d-none");
+        focusPanel[0].classList.remove("d-none");
+        focusPanel[0].classList.add("active-panel");
+        console.log(activePanel);
+        
+        }))
+
+    }
+
+    previousMenu () {
+        this.backToMainPage = document.querySelectorAll(".backToMainPage");
+        this.backToMainPage.forEach( e => e.addEventListener("click", () => {
+            console.log("witaj")
+            this.mainPage.classList.remove("d-none");
+            this.updatePanelPage.classList.add("d-none");
+        }))
+    }
 }
 
 const controlapp = new ControlPanel();
